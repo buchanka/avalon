985c93dcd3910ffaacf63d6188892e36be28e248
buchanka <ninagileva14@gmail.com>
2025-06-03 04:25:35 +0300

    исправление vite config2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   esbuild: {
-    jsxInject: `import React from 'react'`, 
-  },
+  plugins: [react()],
   base: 'avalon'
 
 });
